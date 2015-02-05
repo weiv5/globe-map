35945743dce54c98c9eb7ca4409b405b02a2899c
weiwei <wei@wei.local>
2015-02-05 16:45:30 +0800

    format

--- globe.js
+++ globe.js
@@ -28,7 +28,7 @@ DAT.Globe = function(container, opts) {
             uniforms: {
                 'texture': {
                     type: 't',
-                    value: null
+                    value: null,
                 }
             },
             vertexShader: [
@@ -50,7 +50,7 @@ DAT.Globe = function(container, opts) {
                 'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
                 'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
                 '}'
-            ].join('\n')
+            ].join('\n'),
         },
         'atmosphere': {
             uniforms: {},
@@ -67,7 +67,7 @@ DAT.Globe = function(container, opts) {
                 'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
                 'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
                 '}'
-            ].join('\n')
+            ].join('\n'),
         }
     };
 
